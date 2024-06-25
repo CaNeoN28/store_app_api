58e92a5ba351e5156f92022d3eff7b28fdda8d35
Carlos Felipe <carlosfelipe.st28@gmail.com>
2024-06-25 10:05:20 -0400

    Adicionando arquivos dos controllers

---
+++ src/controllers/Cliente.controller.ts
@@ -0,0 +1,3 @@
+import Controller from "./Controller";
+
+export default class Controller_Cliente extends Controller {}
---
+++ src/controllers/Fornecedores.controller.ts
@@ -0,0 +1,3 @@
+import Controller from "./Controller";
+
+export default class Controller_Fornecedor extends Controller{}
