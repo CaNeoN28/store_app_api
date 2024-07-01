2f75d56c893362f2ea57441f1bcd108dcc55c58d
Carlos Felipe <carlosfelipe.st28@gmail.com>
2024-07-01 10:29:13 -0400

    Validando dados do login

--- src/controllers/Autenticacao.controller.ts
+++ src/controllers/Autenticacao.controller.ts
@@ -59,6 +59,14 @@ export default class Controller_Autenticacao extends Controller {
     const { nome_usuario, senha } = data;
     const erros: { [k: string]: string } = {};
 
+    if (!nome_usuario) {
+      erros.nome_usuario = "Nome de usuário é obrigatório";
+    }
+
+    if (!senha) {
+      erros.senha = "Senha é obrigatório";
+    }
+
     if (Object.keys(erros).length > 0) {
       throw {
         codigo: 401,
