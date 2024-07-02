fd81f1499fc9cbe44920e56470fb992082ebb1ed
Carlos Felipe <carlosfelipe.st28@gmail.com>
2024-07-02 08:58:12 -0400

    Verificando a existência do usuário no Middleware

--- src/middlewares/authentication.middleware.ts
+++ src/middlewares/authentication.middleware.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { verificar_token_usuario } from "../utils/jwt";
+import prisma from "../db/prisma";
 
 export default async function authentication_middleware(
   req: Request,
@@ -17,9 +18,19 @@ export default async function authentication_middleware(
   if (dados_usuario) {
     const { id, nome_usuario } = dados_usuario;
 
-    (req.user_id = id), (req.user_name = nome_usuario);
+    const usuario = await prisma.usuario.findFirst({
+      where: { id },
+      select: {
+        id: true,
+      },
+    });
 
-    return next();
+    if (usuario) {
+      (req.user_id = id), (req.user_name = nome_usuario);
+      return next();
+    }
+
+    res.status(403).send("Não foi possível realizar a autenticação");
   } else {
     return res.status(403).send("Não foi possível realizar a autenticação");
   }
