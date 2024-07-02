beff698ff889c4e6e35f048fad6916045078646b
Carlos Felipe <carlosfelipe.st28@gmail.com>
2024-07-02 14:20:34 -0400

    Adicionando middleware às rotas de grupo

--- src/routes/grupos.router.ts
+++ src/routes/grupos.router.ts
@@ -1,15 +1,22 @@
 import { Router } from "express";
 import Controller_Grupos from "../controllers/Grupos.controller";
+import authentication_middleware from "../middlewares/authentication.middleware";
 
 const router_grupos = Router();
 const controller = new Controller_Grupos();
 
-router_grupos.route("/grupos").get(controller.list);
-router_grupos.route("/grupo").post(controller.create);
+router_grupos
+  .route("/grupos")
+  .get(authentication_middleware("GRUPO"), controller.list);
+
+router_grupos
+  .route("/grupo")
+  .post(authentication_middleware("GRUPO"), controller.create);
+  
 router_grupos
   .route("/grupo/:id")
-  .patch(controller.update_by_id)
-  .put(controller.update_by_id)
-  .delete(controller.remove_by_id)
+  .patch(authentication_middleware("GRUPO"), controller.update_by_id)
+  .put(authentication_middleware("GRUPO"), controller.update_by_id)
+  .delete(authentication_middleware("GRUPO"), controller.remove_by_id);
 
 export default router_grupos;
