bd3f18aef21ae887af71d80bd90d687bafb21711
Carlos Felipe <carlosfelipe.st28@gmail.com>
2024-07-03 07:39:20 -0400

    Criando o controller de cadastro de fornecedor

--- src/controllers/Fornecedores.controller.ts
+++ src/controllers/Fornecedores.controller.ts
@@ -1,9 +1,9 @@
 import { Prisma } from "@prisma/client";
-import { Fornecedor } from "../types";
+import { Erro, Fornecedor } from "../types";
 import Controller from "./Controller";
 import { RequestHandler } from "express";
-import { ParamsDictionary } from "express-serve-static-core";
-import { ParsedQs } from "qs";
+import validar_cnpj from "../utils/validacao/validar_cnpj";
+import verificar_erro_prisma from "../utils/verificar_erro_prisma";
 
 export default class Controller_Fornecedor extends Controller {
   protected selecionados: Prisma.FornecedorSelect;
@@ -30,7 +30,6 @@ export default class Controller_Fornecedor extends Controller {
       next(err);
     }
   };
-
   protected find_many = async (
     filtros: Prisma.FornecedorWhereInput,
     ordenacao: Prisma.FornecedorOrderByWithRelationInput,
@@ -47,8 +46,73 @@ export default class Controller_Fornecedor extends Controller {
 
     const fornecedores = await this.tabela.findMany(query);
 
-    return fornecedores
+    return fornecedores;
   };
 
-  protected validar_dados(data: Fornecedor, validar_obrigatorios?: boolean) {}
+  create: RequestHandler = async (req, res, next) => {
+    const usuario = req.user!;
+    const { cnpj, nome }: Fornecedor = req.body;
+
+    try {
+      const fornecedor = await this.insert_one({ cnpj, nome }, usuario.id);
+
+      res.status(201).send(fornecedor);
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  protected insert_one = async (data: Fornecedor, id_usuario?: number) => {
+    this.validar_dados(data, true);
+
+    const fornecedor = await this.tabela
+      .create({
+        data: {
+          cnpj: data.cnpj,
+          nome: data.nome,
+        },
+        select: this.selecionados,
+      })
+      .then((res) => res)
+      .catch((err) => {
+        const { codigo, erro } = verificar_erro_prisma(err);
+
+        throw {
+          codigo,
+          erro,
+          mensagem: "Não foi possível cadastrar o fornecedor",
+        } as Erro;
+      });
+
+    return fornecedor;
+  };
+
+  protected validar_dados(data: Fornecedor, validar_obrigatorios?: boolean) {
+    const { cnpj, nome } = data;
+    const erros: {
+      [k: string]: any;
+    } = {};
+
+    if (validar_obrigatorios) {
+      if (!cnpj) {
+        erros.cnpj = "CNPJ é obrigatório";
+      }
+
+      if (!nome) {
+        erros.nome = "Nome é obrigatório";
+      }
+    }
+
+    if (cnpj && !validar_cnpj(cnpj)) {
+      erros.cnpj = "CNPJ é inválido";
+    }
+
+    if (Object.keys(erros).length > 0) {
+      throw {
+        codigo: 400,
+        erro: erros,
+        mensagem: "Erro de validação de grupo",
+      } as Erro;
+    }
+  }
 }
