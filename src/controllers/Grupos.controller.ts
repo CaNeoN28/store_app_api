8f5381bdcba71df2d5048cb583d367880ed80130
Carlos Felipe <carlosfelipe.st28@gmail.com>
2024-07-03 10:46:59 -0400

    Removendo métodos do controller de grupos

--- src/controllers/Grupos.controller.ts
+++ src/controllers/Grupos.controller.ts
@@ -2,7 +2,7 @@ import Controller from "./Controller";
 import { Prisma } from "@prisma/client";
 import { RequestHandler } from "express";
 import verificar_erro_prisma from "../utils/verificar_erro_prisma";
-import { Grupo, Erro } from "../types";
+import { Grupo, Erro, Metodo } from "../types";
 import { METODOS, TABELAS } from "../utils/globals";
 import { validar_grupo } from "../utils/validacao";
 
@@ -35,10 +35,18 @@ export default class Controller_Grupos extends Controller {
   }
 
   list: RequestHandler = async (req, res, next) => {
-    const { nome, ordenar, limite, pagina } = req.query;
+    const { nome, ordenar } = req.query;
+    let limite = Number(req.query.number),
+      pagina = Number(req.query.pagina);
+
+    if (isNaN(pagina)) {
+      pagina = Controller.PAGINA_EXIBICAO_PADRAO;
+    }
+    if (isNaN(limite)) {
+      limite = Controller.LIMITE_EXIBICAO_PADRAO;
+    }
+
     const filtros: Prisma.GrupoWhereInput = {};
-    const ordenacao: Prisma.GrupoOrderByWithRelationInput =
-      this.formatar_ordenacao(ordenar) as Prisma.GrupoOrderByWithRelationInput;
 
     if (nome) {
       filtros.nome = {
@@ -47,31 +55,8 @@ export default class Controller_Grupos extends Controller {
       };
     }
 
-    try {
-      const resposta = await this.find_many(
-        filtros,
-        ordenacao,
-        Number(limite),
-        Number(pagina)
-      );
-
-      res.status(200).send(resposta);
-    } catch (err) {
-      next(err);
-    }
-  };
-  protected find_many = async (
-    filtros: Prisma.GrupoWhereInput,
-    ordenacao: Prisma.GrupoOrderByWithRelationInput,
-    limite: number,
-    pagina: number
-  ) => {
-    if (isNaN(pagina)) {
-      pagina = Controller.PAGINA_EXIBICAO_PADRAO;
-    }
-    if (isNaN(limite)) {
-      limite = Controller.LIMITE_EXIBICAO_PADRAO;
-    }
+    const ordenacao: Prisma.GrupoOrderByWithRelationInput =
+      this.formatar_ordenacao(ordenar) as Prisma.GrupoOrderByWithRelationInput;
 
     const query = Controller.definir_query(
       filtros,
@@ -81,38 +66,30 @@ export default class Controller_Grupos extends Controller {
       pagina
     );
 
-    const registros = await this.tabela.count({ where: filtros });
-    const resultado = await this.tabela.findMany(query);
-
-    const maximo_paginas =
-      registros > 0 ? 1 + Math.floor(registros / limite) : 0;
-
-    return {
-      resultado,
-      pagina,
-      maximo_paginas,
-      registros,
-      limite,
-    };
-  };
-
-  create: RequestHandler = async (req, res, next) => {
-    const { nome, acessos, usuarios }: Grupo = req.body;
-
     try {
-      const resposta = await this.insert_one({ acessos, nome, usuarios });
+      const registros = await this.tabela.count({ where: filtros });
+      const grupos = await this.tabela.findMany(query);
 
-      res.status(201).send(resposta);
+      const maximo_paginas =
+        registros > 0 ? 1 + Math.floor(registros / limite) : 0;
+
+      res.status(200).send({
+        resultado: grupos,
+        pagina,
+        maximo_paginas,
+        registros,
+        limite,
+      });
     } catch (err) {
       next(err);
     }
   };
-  protected insert_one = async (data: Grupo) => {
-    const { acessos, nome, usuarios } = data;
-
-    validar_grupo({ nome, acessos, usuarios }, true);
+  create: RequestHandler = async (req, res, next) => {
+    const { nome, acessos, usuarios }: Grupo = req.body;
 
     try {
+      validar_grupo({ acessos, nome, usuarios });
+
       const grupo = await this.tabela
         .create({
           data: {
@@ -143,212 +120,182 @@ export default class Controller_Grupos extends Controller {
           },
           select: this.selecionados,
         })
-        .then((res) => res);
+        .then((res) => res)
+        .catch((err) => {
+          const { codigo, erro } = verificar_erro_prisma(err);
 
-      return grupo;
-    } catch (err) {
-      const { codigo, erro } = verificar_erro_prisma(err);
+          throw {
+            codigo,
+            erro,
+            mensagem: "Não foi possível criar o grupo",
+          } as Erro;
+        });
 
-      throw {
-        mensagem: "Não foi possível criar grupo",
-        codigo,
-        erro,
-      } as Erro;
-    }
-  };
-
-  update_by_id: RequestHandler = async (req, res, next) => {
-    const id = Number(req.params.id);
-    const { acessos, nome, usuarios }: Grupo = req.body;
-    const metodo = req.method as "PATCH" | "PUT";
-
-    const data = {
-      acessos,
-      nome,
-      usuarios,
-    };
-
-    try {
-      const resposta =
-        metodo == "PATCH"
-          ? await this.update_one(id, data)
-          : metodo == "PUT" && (await this.upsert_one(id, data));
-
-      res.status(200).send(resposta);
+      res.status(201).send(grupo);
     } catch (err) {
       next(err);
     }
   };
-  protected update_one = async (id: number, data: Grupo) => {
-    Controller.validar_id(id);
-    validar_grupo(data);
+  update_by_id: RequestHandler = async (req, res, next) => {
+    const id = Number(req.params.id);
+    const { acessos, nome, usuarios }: Grupo = req.body;
 
-    const { acessos, nome, usuarios } = data;
+    const metodo = req.method as Metodo;
 
-    const grupo_novo = await this.tabela
-      .update({
-        where: { id },
-        data: {
-          nome: nome,
-          acessos: {
-            set: [],
-            connectOrCreate:
-              acessos &&
-              acessos.map((a) => ({
-                where: {
-                  tabela_metodo: {
-                    metodo: a.metodo,
-                    tabela: a.tabela,
-                  },
-                },
-                create: {
-                  metodo: a.metodo,
-                  tabela: a.tabela,
-                },
-              })),
-          },
-          usuarios: {
-            connect:
-              usuarios &&
-              usuarios.map((u) => ({
-                id: u.id,
-              })),
-          },
-        },
-        select: this.selecionados,
-      })
-      .then((res) => res)
-      .catch((err) => {
-        const { codigo, erro } = verificar_erro_prisma(err);
+    try {
+      let grupo: any = undefined;
 
-        throw {
-          codigo,
-          erro,
-          mensagem: "Não foi possível atualizar o grupo",
-        } as Erro;
-      });
+      if (metodo == "PATCH") {
+        grupo = await this.tabela
+          .update({
+            where: { id },
+            data: {
+              nome: nome,
+              acessos: {
+                set: [],
+                connectOrCreate:
+                  acessos &&
+                  acessos.map((a) => ({
+                    where: {
+                      tabela_metodo: {
+                        metodo: a.metodo,
+                        tabela: a.tabela,
+                      },
+                    },
+                    create: {
+                      metodo: a.metodo,
+                      tabela: a.tabela,
+                    },
+                  })),
+              },
+              usuarios: {
+                connect:
+                  usuarios &&
+                  usuarios.map((u) => ({
+                    id: u.id,
+                  })),
+              },
+            },
+            select: this.selecionados,
+          })
+          .then((res) => res)
+          .catch((err) => {
+            const { codigo, erro } = verificar_erro_prisma(err);
 
-    if (acessos) {
-      await this.remover_acessos_nao_utilizados();
+            throw {
+              codigo,
+              erro,
+              mensagem: "Não foi possível atualizar o grupo",
+            } as Erro;
+          });
+      } else if (metodo == "PUT") {
+        grupo = await this.tabela
+          .upsert({
+            where: { id },
+            create: {
+              id,
+              nome,
+              acessos: {
+                connectOrCreate:
+                  acessos &&
+                  acessos.map((a) => ({
+                    where: {
+                      tabela_metodo: {
+                        metodo: a.metodo,
+                        tabela: a.tabela,
+                      },
+                    },
+                    create: {
+                      metodo: a.metodo,
+                      tabela: a.tabela,
+                    },
+                  })),
+              },
+              usuarios: {
+                connect:
+                  usuarios &&
+                  usuarios.map((u) => ({
+                    id: u.id,
+                  })),
+              },
+            },
+            update: {
+              nome,
+              acessos: {
+                set: [],
+                connectOrCreate:
+                  acessos &&
+                  acessos.map((a) => ({
+                    where: {
+                      tabela_metodo: {
+                        metodo: a.metodo,
+                        tabela: a.tabela,
+                      },
+                    },
+                    create: {
+                      metodo: a.metodo,
+                      tabela: a.tabela,
+                    },
+                  })),
+              },
+              usuarios: {
+                set: [],
+                connect:
+                  usuarios &&
+                  usuarios.map((u) => ({
+                    id: u.id,
+                  })),
+              },
+            },
+            select: this.selecionados,
+          })
+          .then((res) => res)
+          .catch((err) => {
+            const { codigo, erro } = verificar_erro_prisma(err);
+
+            throw {
+              codigo,
+              erro,
+              mensagem: "Não foi possível salvar o grupo",
+            } as Erro;
+          });
+      }
+
+      if (acessos) {
+        await this.remover_acessos_nao_utilizados();
+      }
+
+      res.status(200).send(grupo);
+    } catch (err) {
+      next(err);
     }
-
-    return grupo_novo;
   };
-  protected upsert_one = async (id: number, data: Grupo) => {
-    Controller.validar_id(id);
-    validar_grupo(data, true);
-
-    const { acessos, nome, usuarios } = data;
-
-    const grupo_novo = await this.tabela
-      .upsert({
-        where: { id },
-        create: {
-          id,
-          nome,
-          acessos: {
-            connectOrCreate:
-              acessos &&
-              acessos.map((a) => ({
-                where: {
-                  tabela_metodo: {
-                    metodo: a.metodo,
-                    tabela: a.tabela,
-                  },
-                },
-                create: {
-                  metodo: a.metodo,
-                  tabela: a.tabela,
-                },
-              })),
-          },
-          usuarios: {
-            connect:
-              usuarios &&
-              usuarios.map((u) => ({
-                id: u.id,
-              })),
-          },
-        },
-        update: {
-          nome,
-          acessos: {
-            set: [],
-            connectOrCreate:
-              acessos &&
-              acessos.map((a) => ({
-                where: {
-                  tabela_metodo: {
-                    metodo: a.metodo,
-                    tabela: a.tabela,
-                  },
-                },
-                create: {
-                  metodo: a.metodo,
-                  tabela: a.tabela,
-                },
-              })),
-          },
-          usuarios: {
-            set: [],
-            connect:
-              usuarios &&
-              usuarios.map((u) => ({
-                id: u.id,
-              })),
-          },
-        },
-        select: this.selecionados,
-      })
-      .then((res) => res)
-      .catch((err) => {
-        const { codigo, erro } = verificar_erro_prisma(err);
-
-        throw {
-          codigo,
-          erro,
-          mensagem: "Não foi possível salvar o grupo",
-        } as Erro;
-      });
-
-    if (acessos) {
-      await this.remover_acessos_nao_utilizados();
-    }
-
-    return grupo_novo;
-  };
-
   remove_by_id: RequestHandler = async (req, res, next) => {
     const id = Number(req.params.id);
 
     try {
-      await this.delete_one(id);
+      await this.tabela
+        .delete({
+          where: {
+            id,
+          },
+        })
+        .then()
+        .catch((err) => {
+          const { codigo, erro } = verificar_erro_prisma(err);
+
+          throw {
+            codigo,
+            erro,
+            mensagem: "Não foi possível remover o grupo",
+          } as Erro;
+        });
+
+      await this.remover_acessos_nao_utilizados();
 
       res.status(204).send();
     } catch (err) {
       next(err);
-    }
-  };
-
-  protected delete_one = async (id: number) => {
-    Controller.validar_id(id);
-
-    try {
-      await this.tabela.delete({
-        where: {
-          id,
-        },
-      });
-
-      await this.remover_acessos_nao_utilizados();
-    } catch (err) {
-      const { codigo, erro } = verificar_erro_prisma(err);
-
-      throw {
-        codigo,
-        erro,
-        mensagem: "Não foi possível remover o grupo",
-      } as Erro;
     }
   };
 
