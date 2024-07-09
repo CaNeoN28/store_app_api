69592fd49a3bc650c489683ace238f202f9eefd7
Carlos Felipe <carlosfelipe.st28@gmail.com>
2024-07-09 08:02:13 -0400

    Criando venda

--- src/controllers/Vendas.controller.ts
+++ src/controllers/Vendas.controller.ts
@@ -2,9 +2,50 @@ import { RequestHandler } from "express";
 import { ParamsDictionary } from "express-serve-static-core";
 import { ParsedQs } from "qs";
 import Controller from "./Controller";
+import { Erro, Venda } from "../types";
+import { Tabela_Venda } from "../db/tabelas";
+import verificar_erro_prisma from "../utils/verificar_erro_prisma";
 
 export default class Controller_Vendas extends Controller {
-    create: RequestHandler = async (req, res, next) => {
-        
-    };
+  create: RequestHandler = async (req, res, next) => {
+    const { itens, cliente_id }: Venda = req.body;
+
+    try {
+      const valor_total = itens
+        .map((i) => i.quantidade * i.valor_combinado)
+        .reduce((prev, curr) => prev + curr);
+
+      const venda = await Tabela_Venda.create({
+        data: {
+          cliente_id,
+          venda_item: {
+            create: itens.map((i) => {
+              const { item_id, quantidade, valor_combinado } = i;
+
+              return {
+                item_id,
+                quantidade,
+                valor_venda: valor_combinado,
+              };
+            }),
+          },
+          valor_total,
+        },
+      })
+        .then((res) => res)
+        .catch((err) => {
+          const { codigo, erro } = verificar_erro_prisma(err);
+
+          throw {
+            codigo,
+            erro,
+            mensagem: "Não foi possível criar a venda",
+          } as Erro;
+        });
+
+      res.status(201).send(venda);
+    } catch (err) {
+      next(err);
+    }
+  };
 }
--- src/routes/venda.router.ts
+++ src/routes/venda.router.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import Controller_Vendas from "../controllers/Vendas.controller";
+import authentication_middleware from "../middlewares/authentication.middleware";
 
 const router_vendas = Router();
 const controller = new Controller_Vendas();
+
+router_vendas
+  .route("/criar-venda")
+  .post(authentication_middleware("VENDA"), controller.create);
 
 export default router_vendas;
