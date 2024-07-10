f2d84ace3fea675e1cfbd6543049e9e510f72372
Carlos Felipe <carlosfelipe.st28@gmail.com>
2024-07-10 10:27:22 -0400

    Verificando o estoque antes de criar a perda

--- src/controllers/Perdas.controller.ts
+++ src/controllers/Perdas.controller.ts
@@ -2,10 +2,11 @@ import { RequestHandler } from "express";
 import { ParamsDictionary } from "express-serve-static-core";
 import { ParsedQs } from "qs";
 import Controller from "./Controller";
-import { Perda } from "../types";
-import { Tabela_Perda } from "../db/tabelas";
+import { Erro, Perda } from "../types";
+import { Tabela_Estoque, Tabela_Perda } from "../db/tabelas";
 import { Prisma } from "@prisma/client";
 import validar_perda from "../utils/validacao/validar_perda";
+import verificar_erro_prisma from "../utils/verificar_erro_prisma";
 
 export default class Controller_Perdas extends Controller {
   create: RequestHandler = async (req, res, next) => {
@@ -13,7 +14,45 @@ export default class Controller_Perdas extends Controller {
 
     try {
       validar_perda({ itens });
-      
+
+      const estoques = await Tabela_Estoque.findMany({
+        where: {
+          item_id: {
+            in: itens.map((i) => i.id),
+          },
+        },
+      }).then((res) => {
+        const estoques: { [k: number]: number } = {};
+
+        res.map((e) => {
+          estoques[e.item_id] = Number(e.quantidade);
+        });
+
+        return estoques;
+      });
+
+      const erros_estoque: { [k: string]: any } = {};
+
+      for (const item of itens) {
+        const { id, quantidade } = item;
+        const estoque = estoques[id];
+
+        if (!estoque) {
+          erros_estoque[id] = "O estoque deste produto não está disponível";
+        } else if (estoque < quantidade) {
+          erros_estoque[id] =
+            "Não há quantia o suficiente no estoque para realizar a operação";
+        }
+      }
+
+      if(Object.keys(erros_estoque).length > 0){
+        throw {
+            codigo: 400,
+            erro: erros_estoque,
+            mensagem: "Não foi possível criar perda"
+        } as Erro
+      }
+
       const perdas = await Tabela_Perda.create({
         data: {
           perda_item: {
@@ -28,7 +67,17 @@ export default class Controller_Perdas extends Controller {
           },
         },
         select: this.selecionar_campos(),
-      });
+      })
+        .then((res) => res)
+        .catch((err) => {
+          const { codigo, erro } = verificar_erro_prisma(err);
+
+          throw {
+            codigo,
+            erro,
+            mensagem: "Não foi possível criar perda",
+          } as Erro;
+        });
 
       res.status(201).send(perdas);
     } catch (err) {
