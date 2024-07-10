090d27be0a7d213211c3e4f3373ef4d227b75ac5
Carlos Felipe <carlosfelipe.st28@gmail.com>
2024-07-10 07:49:04 -0400

    Exibindo o estoque

--- src/controllers/Estoque.controller.ts
+++ src/controllers/Estoque.controller.ts
@@ -1,5 +1,37 @@
+import { Prisma } from "@prisma/client";
 import Controller from "./Controller";
+import { RequestHandler } from "express";
+import { ParamsDictionary } from "express-serve-static-core";
+import { ParsedQs } from "qs";
+import { Tabela_Item } from "../db/tabelas";
 
-export default class Estoque_Controller extends Controller{
-    
+export default class Estoque_Controller extends Controller {
+  list: RequestHandler = async (req, res, next) => {
+    try {
+      const estoque = await Tabela_Item.findMany({
+        select: this.selecionar_campos(),
+        orderBy: {
+          id: "asc",
+        },
+      });
+
+      res.status(200).send(estoque)
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  protected selecionar_campos() {
+    const selecionados: Prisma.ItemSelect = {
+      nome: true,
+      id: true,
+      estoque: {
+        select: {
+          quantidade: true,
+        },
+      },
+    };
+
+    return selecionados;
+  }
 }
--- src/routes/estoque.router.ts
+++ src/routes/estoque.router.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import Estoque_Controller from "../controllers/Estoque.controller";
+import authentication_middleware from "../middlewares/authentication.middleware";
 
-const router_estoque = Router()
-const controller = new Estoque_Controller()
+const router_estoque = Router();
+const controller = new Estoque_Controller();
 
-export default router_estoque
+router_estoque
+  .route("/itens/estoque")
+  .get(authentication_middleware("ESTOQUE"), controller.list);
+
+export default router_estoque;
