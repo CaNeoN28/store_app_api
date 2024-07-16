fcf9ef28b0ae5a7a2484700c2b4a6214f6e685bd
Carlos Felipe <carlosfelipe.st28@gmail.com>
2024-07-16 10:39:37 -0400

    Enviando a imagem por meio da rota GET

--- src/routes/itens.router.ts
+++ src/routes/itens.router.ts
@@ -3,6 +3,9 @@ import Controller_Itens from "../controllers/Itens.controller";
 import authentication_middleware from "../middlewares/authentication.middleware";
 import image_handler from "../middlewares/image_handler.middleware";
 import fileUpload from "express-fileupload";
+import { Erro } from "../types";
+import path from "path";
+import fs from "fs";
 
 const router_itens = Router();
 const controller = new Controller_Itens();
@@ -14,6 +17,13 @@ router_itens
   .post(authentication_middleware("ITEM"), controller.create);
 
 router_itens
+  .route("/item/:id")
+  .get(controller.get_id)
+  .put(authentication_middleware("ITEM"), controller.update_by_id)
+  .patch(authentication_middleware("ITEM"), controller.update_by_id)
+  .delete(authentication_middleware("ITEM"), controller.remove_by_id);
+
+router_itens
   .route("/item/:id/imagem")
   .all(
     fileUpload({
@@ -21,13 +31,34 @@ router_itens
       tempFileDir: "/tmp/",
     })
   )
-  .post(authentication_middleware("ITEM"), image_handler("item"), controller.upload_image);
+  .post(
+    authentication_middleware("ITEM"),
+    image_handler("item"),
+    controller.upload_image
+  );
 
-router_itens
-  .route("/item/:id")
-  .get(controller.get_id)
-  .put(authentication_middleware("ITEM"), controller.update_by_id)
-  .patch(authentication_middleware("ITEM"), controller.update_by_id)
-  .delete(authentication_middleware("ITEM"), controller.remove_by_id);
+router_itens.route("/imagem-item/:caminho").get(async (req, res, next) => {
+  const caminho_item = req.params.caminho;
+
+  try {
+    if (!caminho_item) {
+      throw {
+        codigo: 400,
+        erro: "É necessário informar o caminho do arquivo",
+        mensagem: "Não foi possível recuperar a imagem",
+      } as Erro;
+    }
+
+    const caminho_relativo = path.resolve("./files/item/");
+
+    const caminho_completo = path.join(caminho_relativo, caminho_item);
+
+    res.sendFile(caminho_completo, (err) => {
+      console.log(err);
+    });
+  } catch (err) {
+    next(err);
+  }
+});
 
 export default router_itens;
