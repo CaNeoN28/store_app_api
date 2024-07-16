438b214ff1968ed765ecf7d5f0b1c8ec7c95c9d2
Carlos Felipe <carlosfelipe.st28@gmail.com>
2024-07-16 09:16:13 -0400

    Verificando a existência de uma imagem

--- src/middlewares/file_handler.middleware.ts
+++ src/middlewares/file_handler.middleware.ts
@@ -5,7 +5,26 @@ export default async function file_handler(
   res: Response,
   next: NextFunction
 ) {
-  console.log(req.files);
+  let erros: { codigo: number; mensagem: string; erro: any } | undefined =
+    undefined;
 
-  res.status(200).send();
+  if (!req.files || !req.files.image) {
+    erros = {
+      codigo: 400,
+      erro: "É necessário enviar uma imagem",
+      mensagem: "Não foi possível realizar a operação",
+    };
+  } else {
+    const { image } = req.files;
+
+    console.log(image);
+  }
+
+  if (erros) {
+    const { codigo, erro, mensagem } = erros;
+    res.status(codigo).send({
+      erro,
+      mensagem,
+    });
+  }
 }
