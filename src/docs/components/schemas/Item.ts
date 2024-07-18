34966b0ee166f411d444bf283c392b8bc8ce64de
Carlos Felipe <carlosfelipe.st28@gmail.com>
2024-07-18 10:10:50 -0400

    Adicionando modelo de item

---
+++ src/docs/components/schemas/Item.ts
@@ -0,0 +1,66 @@
+import { Schema } from "swagger-jsdoc";
+
+const Item_Schema: Schema = {
+  type: "object",
+  properties: {
+    id: {
+      type: "number",
+      obs: `ID gerado pelo banco de dados`,
+    },
+    nome: {
+      type: "string",
+      required: true,
+      obs: `Nome de identificação do item.`,
+    },
+    valor_atual: {
+      type: "number",
+      required: true,
+      default: 0,
+      obs: `Valor do item.
+            Deve ser maior ou iqual a zero.`,
+    },
+    desconto_porcentagem: {
+      type: "number",
+      required: true,
+      default: true,
+      obs: `Desconto atual do item.
+            Deve ser maior ou igual a zero.`,
+    },
+    validade_desconto: {
+      type: "Date",
+      required: false,
+      obs: `Informa o limite para o vencimento do desconto atual.`,
+    },
+    imagem_url: {
+      type: "string",
+      required: false,
+      obs: `Endereço da imagem do item.`,
+    },
+    unidade_id: {
+      type: "string",
+      required: true,
+      return: false,
+      obs: `ID da unidade de medida do item`,
+    },
+    unidade: {
+      type: "object",
+      required: false,
+      obs: `Informações a respeito da unidade do item.
+            Só é utilizado na exibição de dados do item`,
+      $ref: "#/components/schemas/Unidade",
+    },
+    estoque: {
+      type: "object",
+      $ref: "#/components/schemas/Estoque",
+    },
+    alteracoes: {
+      type: "array",
+      obs: `Lista com as alterações realizados no item.`,
+      items: {
+        $ref: "#/compnents/schemas/Alteracao_Item",
+      },
+    },
+  },
+};
+
+export { Item_Schema };
--- src/docs/components/schemas/index.ts
+++ src/docs/components/schemas/index.ts
@@ -5,12 +5,14 @@ import { Grupo_Schema } from "./Grupos";
 import { Usuario_Schema } from "./Usuario";
 import { Fornecedor_Schema } from "./Fornecedor";
 import { Cliente_Schema } from "./Cliente";
+import { Item_Schema } from "./Item";
 
 const schemas: { [k: string]: Schema } = {
   Acesso: Acesso_Schema,
   Cliente: Cliente_Schema,
   Fornecedor: Fornecedor_Schema,
   Grupo: Grupo_Schema,
+  Item: Item_Schema,
   Unidade: Unidade_Schema,
   Usuario: Usuario_Schema,
 };
